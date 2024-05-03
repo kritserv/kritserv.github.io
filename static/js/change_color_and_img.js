88e06f81abe15f8ecc169dd38d27457412da7d79
kritserv <krit.serv@gmail.com>
2024-05-03 20:04:01 +0700

    change resume to experience & add footer

--- static/js/change_color_and_img.js
+++ static/js/change_color_and_img.js
@@ -1,12 +1,16 @@
 function changeColAndImg() {
 	var img = document.getElementById("switchCol");
+	var footer = document.querySelector(".footer");
+
 	if (img.src == "https://images.unsplash.com/photo-1530128118208-89f6ce02b37b") {
-		img.src = "https://images.unsplash.com/photo-1489100517551-92a468b736f0";
+		img.src = "https://images.unsplash.com/photo-1584381443170-bfbc4463966a";
 		document.body.style.backgroundColor = "black";
-		document.documentElement.style.backgroundColor = "black"
+		document.documentElement.style.backgroundColor = "black";
+		footer.style.backgroundColor = "#00a1ff";
 	} else {
 		img.src = "https://images.unsplash.com/photo-1530128118208-89f6ce02b37b";
 		document.body.style.backgroundColor = "#25282c";
-		document.documentElement.style.backgroundColor = "#25282c"
+		document.documentElement.style.backgroundColor = "#25282c";
+		footer.style.backgroundColor = "red";
 	}
 }
