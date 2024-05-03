eae622f07655eab31105d8cf8006e4f31f781ea0
kritserv <krittipoom@outlook.com>
2024-05-03 14:15:28 +0700

    close dialog on background and check for click inside-dialog div & add inside-dialog div

--- static/js/close_dialog_on_background_click.js
+++ static/js/close_dialog_on_background_click.js
@@ -1,14 +1,14 @@
-const modalLightDismiss = ( () => {
-const dialogs = document.querySelectorAll('dialog');
-for (const dialog of dialogs) {
+const modalLightDismiss = (() => {
+  const dialogs = document.querySelectorAll('dialog');
+  for (const dialog of dialogs) {
 	dialog.addEventListener('click', (event) => {
-		if (event.target === dialog) dialog.close()
+	  if (event.target === dialog) dialog.close();
 	});
 
 	// Stop propagation for clicks inside the dialog
-	const dialogContent = dialog.querySelector('h1');
+	const dialogContent = dialog.querySelector('.inside-dialog');
 	dialogContent.addEventListener('click', (event) => {
-		event.stopPropagation();
+	  event.stopPropagation();
 	});
-}
+  }
 })();
