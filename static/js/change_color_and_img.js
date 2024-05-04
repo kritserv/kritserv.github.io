b659d972b4af95d2d06bf90f4d8ce84fd6fa7a07
kritserv <krit.serv@gmail.com>
2024-05-04 19:17:01 +0700

    change theme color from red to gold

--- static/js/change_color_and_img.js
+++ static/js/change_color_and_img.js
@@ -3,7 +3,7 @@ function changeColAndImg() {
 	var footer = document.querySelector(".footer");
 	var dialoglinks = document.getElementsByClassName("dLink");
 
-	if (img.src == "https://images.unsplash.com/photo-1530128118208-89f6ce02b37b") {
+	if (img.src == "https://images.unsplash.com/photo-1485075792961-9ea033ad04fc") {
 		img.src = "https://images.unsplash.com/photo-1584381443170-bfbc4463966a";
 		document.body.style.backgroundColor = "black";
 		document.documentElement.style.backgroundColor = "black";
@@ -12,12 +12,12 @@ function changeColAndImg() {
 			dialoglinks[i].style.color = "#00a1ff";
 		}
 	} else {
-		img.src = "https://images.unsplash.com/photo-1530128118208-89f6ce02b37b";
+		img.src = "https://images.unsplash.com/photo-1485075792961-9ea033ad04fc";
 		document.body.style.backgroundColor = "#25282c";
 		document.documentElement.style.backgroundColor = "#25282c";
 		footer.style.backgroundColor = "#111";
 		for (var i = 0; i < dialoglinks.length; i++) {
-			dialoglinks[i].style.color = "#999";
+			dialoglinks[i].style.color = "gold";
 		}
 	}
 }
